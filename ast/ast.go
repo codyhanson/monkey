8a409c733740bc8349b6b5dcd0da6984ffacf3ee
Cody Hanson <cody@clh.io>
2019-02-17 21:53:11 -0700

    Parse !- Prefix expressions

--- ast/ast.go
+++ ast/ast.go
@@ -20,6 +20,17 @@ type Expression interface {
 	expressionNode()
 }
 
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+type PrefixExpression struct {
+	Token token.Token
+	Operator string
+	Right Expression
+}
+
 type Program struct {
 	Statements []Statement
 }
@@ -99,4 +110,19 @@ func (es *ExpressionStatement) String() string {
 	}
 	return ""
 
+}
+
+func (il *IntegerLiteral) expressionNode() {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String () string { return il.Token.Literal }
+
+func (pe *PrefixExpression) expressionNode() {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String () string {
+	var out bytes.Buffer
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+	return out.String()
 }
